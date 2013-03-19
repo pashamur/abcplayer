56b9eccacd04a79c3e8ddb023ae71c7228d36a5e
Qian Lin <linqian@mit.edu>
2013-03-19 03:32:53 -0400

    add comment to Music and Token

--- src/player/Music.java
+++ src/player/Music.java
@@ -8,6 +8,10 @@ public class Music implements ABCmusic {
     public <R> R accept(Visitor<R> mu) {
         return mu.on(this);
     }
+    /**
+     * Generate a AST with music as the root.
+     * @param tk each element of tk represents a voice.
+     */
     public Music(List<List<Token>> tk) {
         int len=tk.size();
         voices=new ArrayList<ABCmusic>();
--- src/player/Token.java
+++ src/player/Token.java
@@ -13,51 +13,64 @@ public class Token {
         repeat_start, repeat_end, 
         rest, note;
     }
-
     public final Type type;
-    private List<String> text;
-    private int value;
-    private Note note;
-    private Rest rest;
+    private final List<String> text;
+    private final int value; //used for type nth_repeat and tuplet_spec
+    private final Note note; //used for type note
+    private final Rest rest; //used for type rest
     @SuppressWarnings("serial")
+    //map from accidental symbol to integer representation
     private static final Map<String, Integer> accidentalToInt = Collections
-            .unmodifiableMap(new HashMap<String, Integer>() {
-                {
-                    put("^^", 2);
-                    put("^", 1);
-                    put("=", 0);
-                    put("_", -1);
-                    put("__", -2);
-                }
-            });
-
+            .unmodifiableMap(new HashMap<String, Integer>() {{
+                put("^^", 2); put("^", 1); put("=", 0);put("_", -1);put("__", -2);
+            }});
+    
+    /** construct a Token given type and the string s representing the Token.
+     *  the specific type must match string s as specified in grammar.
+     *  matching are (lexer is responsible to checking this syntax)
+     *  t --- s
+     *  simple_bar --- {"|"}
+     *  repeat_start --- {"|:"}
+     *  repeat_end --- {":|"}
+     *  major_section_start --- {"[|"}
+     *  major_section_stop --- {"||"} or {"|]"}
+     *  multinote_start --- {"["}
+     *  multinote_end --- {"]"}
+     *  tuplet_spec --- {"(2"} or {"(3"} or {"(4"}
+     *  nth_repeat --- {"[1"} or {"[2"}
+     *  rest --- {"z"(,"[0-9]*"(,"/","[0-9]*"))} () means optional
+     *  note --- {"^^|^|=|_|__","[a-gA-G]","[']*|[,]*"(,"[0-9]*"(,"/","[0-9]*"))}
+     *  
+     * @param t type of the token
+     * @param s string representing the token.
+     */
     public Token(Type t, List<String> s) {
         type = t;
         text = new ArrayList<String>(s);
         if (type == Type.tuplet_spec) {
             value = (int) (s.get(0).charAt(1) - '0');
+            rest=null;
+            note=null;
             if ( value<2 || value>4 ) throw new RuntimeException("Tuplet-sec value out of bound.");
         }
         else if (type == Type.nth_repeat) {
             value = (int) (s.get(0).charAt(1) - '0');
+            rest=null;
+            note=null;
             if ( value<1 || value>2 ) throw new RuntimeException("nth-repeat value out of bound.");            
         }            
         else {
             value = -1;
             int len = s.size();
-            if (type == Type.rest) {
-                note = null;
-                rest = new Rest(toLength(s.subList(1, len)));
-            }
-            if (type == Type.note) {
-                rest = null;
-                note = toNote(s);
-            }
+            if (type == Type.rest) rest = new Rest(toLength(s.subList(1, len)));
+            else rest=null;
+            if (type == Type.note) note = toNote(s);
+            else note=null;
         }
     }
 
     /**
-     * Convert List<String> s representing a rational number to a Rational.
+     * [Helper] Convert List<String> s representing a rational number to a Rational.
      * 
      * @param s
      *            s.size()=0, 1 or 3. if size=0, return 1/1; 
@@ -107,14 +120,14 @@ public class Token {
     }
 
     /**
-     * s represents note. s.size()=3, 4, or 6. s[0] represents accidental (must
+     * [Helper] s represents note. s.size()=3, 4, or 6. s[0] represents accidental (must
      * be "^^", "^", "=", "_", "__", or empty string which means no accidental
      * is specified). s[1] represents basenote, [a-gA-G]. s[2] repreesnts
      * octave, ("'"+) | (","+), or empty string which means not octave is
      * specified. if size>3, the rest represents length.
      * 
-     * @param s
-     * @return Note
+     * @param s note and its accidental, octave, length strings
+     * @return Note 
      */
     private Note toNote(List<String> s) {
         int len = s.size();
@@ -149,19 +162,34 @@ public class Token {
         }
         return new Note(v,a,o,b,toLength(s.subList(3, len)));
     }
+    /**
+     * @return this.value
+     * @throws RuntimeExeption if type is not nth_repeat or tuplet_spec.
+     */
     public int getValue() {
         if (type.equals(Type.nth_repeat) || type.equals(Type.tuplet_spec)) return value;
         else throw new RuntimeException("Try to get value from a token that is neither nth-spec nor tuplet-spec.");
     }
+    /**
+     * @return a new copy of this.note
+     * @throws RuntimeException if type is not note.
+     */
     public Note getNote() {
         if (type.equals(Type.note)) return note.clone();
         else
             throw new RuntimeException("Try to get note from a token that is not of type note.");
     }
+    /**
+     * @return a new copy of this.rest
+     * @throws RuntimeException if type is not rest.
+     */
     public Rest getRest() {
         if (type.equals(Type.rest)) return rest.clone();
         else throw new RuntimeException("Try to get rest from a token that is not of type rest.");
     }
+    /**
+     * prints text to so.
+     */
     public void print() {
         StringBuilder sb=new StringBuilder();
         for (int i=0;i<text.size();i++) sb.append(text.get(i));
