8e0303ae8471af9ee52405eec8e95c50f4c5aea4
Pasha Muravyev <pashamur@mit.com>
2013-03-31 15:51:51 -0400

    small refactoring of token

--- src/lexer/Token.java
+++ src/lexer/Token.java
@@ -67,7 +67,7 @@ public class Token {
         else {
             value = -1;
             int len = s.size();
-            if (type == Type.rest) rest = new Rest(toLength(s.subList(1, len)));
+            if (type == Type.rest) rest = new Rest(stringToRational(s.subList(1, len)));
             else rest=null;
             if (type == Type.note) note = toNote(s);
             else note=null;
@@ -77,7 +77,7 @@ public class Token {
     /**
      * [Helper] Convert List<String> s representing a rational number to a Rational.
      * 
-     * @param s
+     * @param rationalString
      *            s.size()=0, 1 or 3. if size=0, return 1/1; 
      *            if size=1, first element represents an
      *            integer. if size=3, first element represents an valid, nonzero
@@ -85,40 +85,45 @@ public class Token {
      *            element is "/"; third element is a valid, nonzero denominator,
      *            or an empty string which by default means 2;
      * 
-     * @return Rational
+     * @return Rational The Rational form of the passed in fraction
      * @throws RuntimeException
-     *             if s.size() is not 1 or 3, or does not match the above
+     *             if s.size() is not 0, 1 or 3, or does not match the above
      *             description
      */
-    private Rational toLength(List<String> s) {
-        int len = s.size();
-        if (len==0) return new Rational(1,1);
-        if (len == 1) {
-            if (s.get(0).matches("[1-9]+[0-9]*"))
-                return new Rational(Integer.parseInt(s.get(0)), 1);
+    private Rational stringToRational(List<String> rationalString) {
+        int len = rationalString.size();
+        
+        if (len == 0) 
+        	return new Rational(1,1);
+        else if (len == 1) {
+            if (rationalString.get(0).matches("[1-9]+[0-9]*"))
+                return new Rational(Integer.parseInt(rationalString.get(0)), 1);
             throw new RuntimeException("Integer length notation incorrect.");
         }
-        if (len == 3) {
-            String temp;
+        else if (len == 3) {
             int numerator;
             int denominator;
-            temp = s.get(0);
-            if (temp.length() == 0)
+            
+            String numeratorString = rationalString.get(0);
+            if (numeratorString.length() == 0)
                 numerator = 1;
-            else if (temp.matches("[1-9]+[0-9]*"))
-                numerator = Integer.parseInt(temp);
+            else if (numeratorString.matches("[1-9]+[0-9]*"))
+                numerator = Integer.parseInt(numeratorString);
             else
                 throw new RuntimeException("Numerator notation incorrect.");
-            temp = s.get(1);
-            if (!temp.equals("/"))
+            
+            String divisionSignString = rationalString.get(1);
+            if (!divisionSignString.equals("/"))
                 throw new RuntimeException("Rational / notation incorrect.");
-            temp = s.get(2);
-            if (temp.length() == 0)
+            
+            String denominatorString = rationalString.get(2);
+            if (denominatorString.length() == 0)
                 denominator = 2;
-            else if (temp.matches("[1-9]+[0-9]*"))
-                denominator = Integer.parseInt(temp);
+            else if (denominatorString.matches("[1-9]+[0-9]*"))
+                denominator = Integer.parseInt(denominatorString);
             else
-                throw new RuntimeException("Numerator notation incorrect.");
+                throw new RuntimeException("Denominator notation incorrect.");
+            
             return new Rational(numerator, denominator);
         }
         throw new RuntimeException("Length input size incorrect.");
@@ -127,45 +132,57 @@ public class Token {
     /**
      * [Helper] s represents note. s.size()=3, 4, or 6. s[0] represents accidental (must
      * be "^^", "^", "=", "_", "__", or empty string which means no accidental
-     * is specified). s[1] represents basenote, [a-gA-G]. s[2] repreesnts
+     * is specified). s[1] represents basenote, [a-gA-G]. s[2] represents
      * octave, ("'"+) | (","+), or empty string which means not octave is
      * specified. if size>3, the rest represents length.
      * 
-     * @param s note and its accidental, octave, length strings
+     * @param noteParts note and its accidental, octave, length strings
      * @return Note 
      */
-    private Note toNote(List<String> s) {
-        int len = s.size();
-        boolean b;
-        int a;
-        int o=0;
-        char v;
-        String temp;
-        if (len!=3 && len!=4 && len!=6) throw new RuntimeException("List of string for note is incorrect.");
-        temp = s.get(0);
-        if (temp.length() == 0) {
-            b = false;
-            a = 0;
-        } else {
-            b = true;
-            if (accidentalToInt.containsKey(temp)) a = accidentalToInt.get(temp);
+    private Note toNote(List<String> noteParts) {
+        int len = noteParts.size();
+        boolean hasAccidental;
+        int accidental;
+        int octave=0;
+        char basenote;
+        
+        if (len!=3 && len!=4 && len!=6) 
+        	throw new RuntimeException("List of string for note is incorrect.");
+        
+        String accidentalString = noteParts.get(0);
+        if (accidentalString.length() == 0) {
+            hasAccidental = false;
+            accidental = 0;
+        } 
+        else {
+            hasAccidental = true;
+            if (accidentalToInt.containsKey(accidentalString)) accidental = accidentalToInt.get(accidentalString);
             else throw new RuntimeException("Accidental syntax error.");
         }
-        temp=s.get(1);
-        if (temp.matches("[a-g]")) {
-            o++;
-            v=(char) (temp.charAt(0)-32);
+        String basenoteString = noteParts.get(1);
+        
+        if (basenoteString.matches("[a-g]")) {
+            octave++;
+            basenote=(char) (basenoteString.charAt(0)-32);
         }
-        else if (temp.matches("[A-G]")) v=temp.charAt(0);
-        else throw new RuntimeException("Basenote unrecognized.");
-        temp=s.get(2);
-        int tempLen=temp.length();
-        if (tempLen!=0) {
-            if (temp.matches("[']+")) o+=tempLen;
-            else if (temp.matches("[,]+")) o-=tempLen;
-            else throw new RuntimeException("Octave syntax error.");
+        else if (basenoteString.matches("[A-G]")) 
+        	basenote=basenoteString.charAt(0);
+        else 
+        	throw new RuntimeException("Basenote unrecognized.");
+        
+        String octaveString = noteParts.get(2);
+        int numberOfOctaves = octaveString.length();
+        if (numberOfOctaves != 0) {
+            if (octaveString.matches("[']+")) 
+            	octave+=numberOfOctaves;
+            else if (octaveString.matches("[,]+")) 
+            	octave-=numberOfOctaves;
+            else 
+            	throw new RuntimeException("Octave syntax error.");
         }
-        return new Note(v,o,a,b,toLength(s.subList(3, len)));
+        Rational noteLength = stringToRational(noteParts.subList(3, len));
+        
+        return new Note(basenote,octave,accidental,hasAccidental,noteLength);
     }
     /**
      * @return this.value
