ea9a826c37c8dca24f6df5592f0a3a1741179788
Chen <zplghyy@gmail.com>
2013-04-01 01:36:40 -0400

    41 2

--- src/lexer/HeaderTest.java
+++ src/lexer/HeaderTest.java
@@ -1,33 +1,93 @@
 package lexer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.junit.Test;
+
 import abcmusic.Pair;
+
 import player.Rational;
-import test.TestHelpers;
 
 public class HeaderTest {
-	@Test
+    @Test
     public void HeaderTest1() throws IOException{
-        Header header = TestHelpers.getFileHeader("sample_abc/invention.abc");
-        
+        String file="sample_abc/invention.abc";
+        List<String> result = new ArrayList<String>();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new IOException("Cannot find the file");
+        }
+        BufferedReader reader = new BufferedReader(fileReader);
+        String temp;
+        int head = 0;
+        Header header = null;
+        while ((temp = reader.readLine()) != null) {
+            Pattern commentPattern = Pattern.compile("%[\\w\\s]*");
+            if ((!commentPattern.matcher(temp).matches()) && (!temp.equals(""))) {
+                result.add(temp);
+            }
+            if ((!(temp.equals(""))) && (temp.substring(0, 1).equals("K"))
+                    && (head == 0)) {
+                head = 1;
+                header = new Header(result);
+                result = new ArrayList<String>();
+            }
+        }
+        reader.close();
         assertTrue(header.getDefaultNoteLength().equals(new Rational(1,8)));
         assertTrue(header.getMeter().equals(new Pair<Integer,Integer>(4,4)));
         assertEquals(header.getTempo(),140);
         assertEquals(header.getNumVoices(),2);
         assertEquals(header.getVoiceIndex("1"),1);
         assertEquals(header.getVoiceIndex("2"),2);
-        
-        int[] expectedKeySignature= {0,0,0,0,0,0,0};
-        assertArrayEquals(expectedKeySignature, header.getKeySignature()); 
+        int[] keySign = new int[7];
+        for (char i='A';i<'G';i++ ) {
+           keySign[i-'A']=header.getAccidental(i); 
+        }
+        int[] expected= {0,0,0,0,0,0,0};
+        assertArrayEquals(expected,keySign); 
         
          }
 
     @Test
     public void HeaderTest2() throws IOException{
-    	Header header = TestHelpers.getFileHeader("sample_abc/prelude.abc");
-        
+        String file="sample_abc/prelude.abc";
+        List<String> result = new ArrayList<String>();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new IOException("Cannot find the file");
+        }
+        BufferedReader reader = new BufferedReader(fileReader);
+        String temp;
+        int head = 0;
+        Header header = null;
+        while ((temp = reader.readLine()) != null) {
+            Pattern commentPattern = Pattern.compile("%[\\w\\s]*");
+            if ((!commentPattern.matcher(temp).matches()) && (!temp.equals(""))) {
+                result.add(temp);
+            }
+            if ((!(temp.equals(""))) && (temp.substring(0, 1).equals("K"))
+                    && (head == 0)) {
+                head = 1;
+                header = new Header(result);
+                result = new ArrayList<String>();
+            }
+        }
+        reader.close();
         assertTrue(header.getDefaultNoteLength().equals(new Rational(1,16)));
         assertTrue(header.getMeter().equals(new Pair<Integer,Integer>(4,4)));
         assertEquals(header.getTempo(),280);
@@ -35,39 +95,212 @@ public class HeaderTest {
         assertEquals(header.getVoiceIndex("1"),1);
         assertEquals(header.getVoiceIndex("2"),2);
         assertEquals(header.getVoiceIndex("3"),3);        
+        int[] keySign = new int[7];
+        for (char i='A';i<'G';i++ ) {
+           keySign[i-'A']=header.getAccidental(i); 
+        }
+        int[] expected= {0,0,0,0,0,0,0};
+        assertArrayEquals(expected,keySign); 
+        
 
-        int[] expectedKeySignature= {0,0,0,0,0,0,0};
-        assertArrayEquals(expectedKeySignature, header.getKeySignature()); 
-    }
+               }
     
  
     @Test
     public void HeaderTest3() throws IOException{
-    	Header header = TestHelpers.getFileHeader("sample_abc/little_night_music.abc");
-       
+        String file="sample_abc/little_night_music.abc";
+        List<String> result = new ArrayList<String>();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new IOException("Cannot find the file");
+        }
+        BufferedReader reader = new BufferedReader(fileReader);
+        String temp;
+        int head = 0;
+        Header header = null;
+        while ((temp = reader.readLine()) != null) {
+            Pattern commentPattern = Pattern.compile("%[\\w\\s]*");
+            if ((!commentPattern.matcher(temp).matches()) && (!temp.equals(""))) {
+                result.add(temp);
+            }
+            if ((!(temp.equals(""))) && (temp.substring(0, 1).equals("K"))
+                    && (head == 0)) {
+                head = 1;
+                header = new Header(result);
+                result = new ArrayList<String>();
+            }
+        }
+        reader.close();
         assertTrue(header.getDefaultNoteLength().equals(new Rational(1,8)));
         assertTrue(header.getMeter().equals(new Pair<Integer,Integer>(4,4)));
         assertEquals(header.getTempo(),280);
-
-        int[] expectedKeySignature= {0,0,0,0,0,1,0};
-        assertArrayEquals(expectedKeySignature, header.getKeySignature()); 
-    }
+        int[] keySign = new int[7];
+        for (char i='A';i<'G';i++ ) {
+           keySign[i-'A']=header.getAccidental(i); 
+        }
+        int[] expected= {0, 0, 0, 0, 0, 1, 0};
+        assertArrayEquals(expected,keySign); 
+        
+         }
 
     
     @Test
     public void HeaderTest4() throws IOException{
-    	Header header = TestHelpers.getFileHeader("sample_abc/fur_elise.abc");
-        
+        String file="sample_abc/fur_elise.abc";
+        List<String> result = new ArrayList<String>();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new IOException("Cannot find the file");
+        }
+        BufferedReader reader = new BufferedReader(fileReader);
+        String temp;
+        int head = 0;
+        Header header = null;
+        while ((temp = reader.readLine()) != null) {
+            Pattern commentPattern = Pattern.compile("%[\\w\\s]*");
+            if ((!commentPattern.matcher(temp).matches()) && (!temp.equals(""))) {
+                result.add(temp);
+            }
+            if ((!(temp.equals(""))) && (temp.substring(0, 1).equals("K"))
+                    && (head == 0)) {
+                head = 1;
+                header = new Header(result);
+                result = new ArrayList<String>();
+            }
+        }
+        reader.close();
         assertTrue(header.getDefaultNoteLength().equals(new Rational(1,16)));
         assertTrue(header.getMeter().equals(new Pair<Integer,Integer>(3,8)));
         assertEquals(header.getTempo(),240);
         assertEquals(header.getNumVoices(),2);
         assertEquals(header.getVoiceIndex("1"),1);
         assertEquals(header.getVoiceIndex("2"),2);
+        int[] keySign = new int[7];
+        for (char i='A';i<'G';i++ ) {
+           keySign[i-'A']=header.getAccidental(i); 
+        }
+        int[] expected= {0,0,0,0,0,0,0};
+        assertArrayEquals(expected,keySign); 
         
-        int[] expectedKeySignature= {0,0,0,0,0,0,0};
-        assertArrayEquals(expectedKeySignature, header.getKeySignature()); 
+         }
+    
+    @Test(expected = RuntimeException.class)
+    public void HeaderTest5() throws IOException{
+        String file="sample_abc/headerTest1.abc";
+        List<String> result = new ArrayList<String>();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new IOException("Cannot find the file");
+        }
+        BufferedReader reader = new BufferedReader(fileReader);
+        String temp;
+        int head = 0;
+        Header header = null;
+        while ((temp = reader.readLine()) != null) {
+            Pattern commentPattern = Pattern.compile("%[\\w\\s]*");
+            if ((!commentPattern.matcher(temp).matches()) && (!temp.equals(""))) {
+                result.add(temp);
+            }
+            if ((!(temp.equals(""))) && (temp.substring(0, 1).equals("K"))
+                    && (head == 0)) {
+                head = 1;
+                header = new Header(result);
+                result = new ArrayList<String>();
+            }
+        }
+        reader.close();
         
     }
+    @Test(expected = RuntimeException.class)
+    public void HeaderTest6() throws IOException{
+        String file="sample_abc/headerTest2.abc";
+        List<String> result = new ArrayList<String>();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new IOException("Cannot find the file");
+        }
+        BufferedReader reader = new BufferedReader(fileReader);
+        String temp;
+        int head = 0;
+        Header header = null;
+        while ((temp = reader.readLine()) != null) {
+            Pattern commentPattern = Pattern.compile("%[\\w\\s]*");
+            if ((!commentPattern.matcher(temp).matches()) && (!temp.equals(""))) {
+                result.add(temp);
+            }
+            if ((!(temp.equals(""))) && (temp.substring(0, 1).equals("K"))
+                    && (head == 0)) {
+                head = 1;
+                header = new Header(result);
+                result = new ArrayList<String>();
+            }
+        }
+        reader.close();
+        
+    }@Test(expected = RuntimeException.class)
+    public void HeaderTest7() throws IOException{
+        String file="sample_abc/headerTest3.abc";
+        List<String> result = new ArrayList<String>();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new IOException("Cannot find the file");
+        }
+        BufferedReader reader = new BufferedReader(fileReader);
+        String temp;
+        int head = 0;
+        Header header = null;
+        while ((temp = reader.readLine()) != null) {
+            Pattern commentPattern = Pattern.compile("%[\\w\\s]*");
+            if ((!commentPattern.matcher(temp).matches()) && (!temp.equals(""))) {
+                result.add(temp);
+            }
+            if ((!(temp.equals(""))) && (temp.substring(0, 1).equals("K"))
+                    && (head == 0)) {
+                head = 1;
+                header = new Header(result);
+                result = new ArrayList<String>();
+            }
+        }
+        reader.close();
+        
+    }@Test(expected = RuntimeException.class)
+    public void HeaderTest8() throws IOException{
+        String file="sample_abc/headerTest4.abc";
+        List<String> result = new ArrayList<String>();
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new IOException("Cannot find the file");
+        }
+        BufferedReader reader = new BufferedReader(fileReader);
+        String temp;
+        int head = 0;
+        Header header = null;
+        while ((temp = reader.readLine()) != null) {
+            Pattern commentPattern = Pattern.compile("%[\\w\\s]*");
+            if ((!commentPattern.matcher(temp).matches()) && (!temp.equals(""))) {
+                result.add(temp);
+            }
+            if ((!(temp.equals(""))) && (temp.substring(0, 1).equals("K"))
+                    && (head == 0)) {
+                head = 1;
+                header = new Header(result);
+                result = new ArrayList<String>();
+            }
+        }
+        reader.close();
+        
+    }    
 }
 
